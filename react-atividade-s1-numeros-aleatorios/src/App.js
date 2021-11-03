4d25d020406ee97ba8b16c2368ec6936225fc7fe
Pombo <luizhfpr@gmail.com>
2021-11-02 23:46:38 -0300

    atividade completa

---
+++ react-atividade-s1-numeros-aleatorios/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [number, setNumber] = useState(Math.floor(Math.random() * 100) + 1);
+  function randomNumber() {
+    setNumber(Math.floor(Math.random() * 100) + 1);
+  }
+  return (
+    <div className="App">
+      <div className="App-header">
+        <span> {number} </span>
+        <button onClick={randomNumber}> Criar novo número </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
